4c83131a17721f0bfcfbcb71b87796480464b7f6
Chris <128728293+DMTumbler@users.noreply.github.com>
2024-04-13 13:57:50 -0400

    Refactor Book class and Main class in Java files

--- vscode/mooc-java-programming-i/part04-Part04_20.Books/src/main/java/Book.java
+++ vscode/mooc-java-programming-i/part04-Part04_20.Books/src/main/java/Book.java
@@ -1,37 +1,37 @@
 public class Book {
-    private String title;
-    private String pages;
-    private String year;
+  private String title;
+  private String pages;
+  private String year;
 
-    public Book(String title, String pages, String year) {
-        this.title = title;
-        this.pages = pages;
-        this.year = year;
+  public Book(String title, String pages, String year) {
+    this.title = title;
+    this.pages = pages;
+    this.year = year;
 
-    }
+  }
 
-    public void setYear(String year) {
-        this.year = year;
-    }
+  public void setYear(String year) {
+    this.year = year;
+  }
 
-    public void setPages(String pages) {
-        this.pages = pages;
-    }
+  public void setPages(String pages) {
+    this.pages = pages;
+  }
 
-    public String getYear() {
-        return this.year;
-    }
+  public String getYear() {
+    return this.year;
+  }
 
-    public String getPages() {
-        return this.pages;
-    }
+  public String getPages() {
+    return this.pages;
+  }
 
-    public String getTitle() {
-        return title;
-    }
+  public String getTitle() {
+    return title;
+  }
 
-    public String toString() {
-        return title + ", " + pages + " pages, " + year;
+  public String toString() {
+    return title + ", " + pages + " pages, " + year;
 
-    }
+  }
 }
--- vscode/mooc-java-programming-i/part04-Part04_20.Books/src/main/java/Main.java
+++ vscode/mooc-java-programming-i/part04-Part04_20.Books/src/main/java/Main.java
@@ -3,44 +3,44 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        String title = "", pages = "", year = "";
-        ArrayList<Book> books = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("Title: ");
-            title = scanner.nextLine();
-            if (title.isEmpty()) {
-                break;
-            }
-            System.out.println("Pages: ");
-            pages = scanner.nextLine();
-            System.out.println("Publication year: ");
-            year = scanner.nextLine();
-            books.add(new Book(title, pages, year));
-        }
-        System.out.println("What information will be printed?");
-        String answer = scanner.nextLine();
-        if (answer.equals("everything")) {
-            for (Book book : books) {
-                System.out.println(book);
-            }
-        } else if (answer.equals("name")) {
-            System.out.println();
-            for (Book book : books) {
-                System.out.println(book.getTitle());
-            }
-        } else if (answer.equals("pages")) {
-            System.out.println();
-            for (Book book : books) {
-                System.out.println(book.getPages());
-            }
-        } else if (answer.equals("year")) {
-            System.out.println();
-            for (Book book : books) {
-                System.out.println(book.getYear());
-            }
-        }
+  public static void main(String[] args) {
+    String title = "", pages = "", year = "";
+    ArrayList<Book> books = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
+    while (true) {
+      System.out.println("Title: ");
+      title = scanner.nextLine();
+      if (title.isEmpty()) {
+        break;
+      }
+      System.out.println("Pages: ");
+      pages = scanner.nextLine();
+      System.out.println("Publication year: ");
+      year = scanner.nextLine();
+      books.add(new Book(title, pages, year));
     }
+    System.out.println("What information will be printed?");
+    String answer = scanner.nextLine();
+    if (answer.equals("everything")) {
+      for (Book book : books) {
+        System.out.println(book);
+      }
+    } else if (answer.equals("name")) {
+      System.out.println();
+      for (Book book : books) {
+        System.out.println(book.getTitle());
+      }
+    } else if (answer.equals("pages")) {
+      System.out.println();
+      for (Book book : books) {
+        System.out.println(book.getPages());
+      }
+    } else if (answer.equals("year")) {
+      System.out.println();
+      for (Book book : books) {
+        System.out.println(book.getYear());
+      }
+    }
+  }
 
 }
