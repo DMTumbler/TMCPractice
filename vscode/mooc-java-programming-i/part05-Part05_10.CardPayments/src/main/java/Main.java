dc4f7aae64e4a1df75188b8c6caf2e6e52687ed4
Chris <128728293+DMTumbler@users.noreply.github.com>
2024-04-21 17:04:44 -0400

    Update Pet and Person classes to include toString() method

--- vscode/mooc-java-programming-i/part05-Part05_10.CardPayments/src/main/java/Main.java
+++ vscode/mooc-java-programming-i/part05-Part05_10.CardPayments/src/main/java/Main.java
@@ -1,8 +1,59 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        // write experimental main programs here
+        String userInput = "";
+        boolean validInput = false;
+        PaymentCard chrisCard = new PaymentCard(200);
+        System.out.println(chrisCard.balance());
+        boolean wentThrough = chrisCard.takeMoney(250);
+        if (!wentThrough) {
+            System.out.println("Like I said. You's a poor fuck.");
+        }
+        System.out.println(chrisCard.balance());
+        wentThrough = chrisCard.takeMoney(60);
+        if (!wentThrough) {
+            System.out.println("The");
+        } else {
+            System.out.println("Huh. You're not so poor after all.");
+        }
+        System.out.println(chrisCard.balance());
+        PaymentTerminal cafe = new PaymentTerminal();
+        int i = 0;
+        while (i < 10) {
+            double change = cafe.eatAffordably(10);
+            System.out.println("The users change is " + change);
+            System.out.println(cafe);
+            change = cafe.eatHeartily(10);
+            System.out.println("The users change is " + change);
+            System.out.println(cafe);
+            cafe.eatAffordably(chrisCard);
+            System.out.println(chrisCard.balance());
+            i++;
+        }
+        System.out.println("Would you like to deposit some money?");
+        Scanner scanner = new Scanner(System.in);
+        userInput = scanner.nextLine().toLowerCase();
+        do {
+            if (userInput.equals("yes") || userInput.equals("y")) {
+                System.out.println("How much would you like to add?");
+                if (scanner.hasNextDouble()) {
+                    chrisCard.addMoney(Integer.valueOf(scanner.nextLine()));
+                    System.out.println(chrisCard.balance());
+                    validInput = true;
+                } else {
+                    System.out.println("That is not a valid amount. Please enter a valid amount.");
+                    scanner.nextLine();
+                }
+            } else if (userInput.equals("no") || userInput.equals("n")) {
+                System.out.println("Thank you! Bye!");
+                validInput = true;
+            } else {
+                System.out.println("That is not a valid input. Enter a yes or no.");
+                userInput = scanner.nextLine();
+            }
+
+        } while (!validInput);
     }
 }
-
--- vscode/mooc-java-programming-i/part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
+++ vscode/mooc-java-programming-i/part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
@@ -1,33 +1,68 @@
 
 public class PaymentTerminal {
 
-
-    private double money;  // amount of cash
+    private double money; // amount of cash
     private int affordableMeals; // number of sold affordable meals
-    private int heartyMeals;  // number of sold hearty meals
-
+    private int heartyMeals; // number of sold hearty meals
 
     public PaymentTerminal() {
-        // register initially has 1000 euros of money
+        this.money = 1000;
     }
 
     public double eatAffordably(double payment) {
-        // an affordable meal costs 2.50 euros
-        // increase the amount of cash by the price of an affordable mean and return the change
-        // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        double cost = 2.50, change = 0;
+        if (payment >= cost) {
+            change = payment - cost;
+            this.money += cost;
+            this.affordableMeals++;
+            return change;
+        }
+        return payment;
     }
 
     public double eatHeartily(double payment) {
-        // a hearty meal costs 4.30 euros
-        // increase the amount of cash by the price of a hearty mean and return the change
-        // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        double cost = 4.30, change = 0;
+        if (payment >= cost) {
+            change = payment - cost;
+            this.money += cost;
+            this.heartyMeals++;
+            return change;
+        }
+        return payment;
     }
 
+    public boolean eatAffordably(PaymentCard card) {
+        double cost = 2.50;
+        if (card.balance() >= cost) {
+            card.takeMoney(cost);
+            this.affordableMeals++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean eatHeartily(PaymentCard card) {
+        double cost = 4.30;
+        if (card.balance() >= cost) {
+            card.takeMoney(cost);
+            this.heartyMeals++;
+            return true;
+        }
+        return false;
+    }
+
+    public void addMoneyToCard(PaymentCard card, double sum) {
+        if (sum >= 0) {
+            if (this.money > 0 || this.money > sum) {
+                card.addMoney(sum);
+                this.money += sum;
+            }
+        }
+    }
 
     @Override
     public String toString() {
-        return "money: " + money + ", number of sold affordable meals: " + affordableMeals + ", number of sold hearty meals: " + heartyMeals;
+        return "money: " + money + ", number of sold affordable meals: " + affordableMeals
+                + ", number of sold hearty meals: " + heartyMeals;
     }
 }
