994508ba3bf5c9ac9d8dbda6c29e22dc35787ae1
Chris <128728293+DMTumbler@users.noreply.github.com>
2024-04-22 13:43:32 -0400

    Add menu functionality to Main.java and Menu.java

--- vscode/mooc-java-programming-i/part06-Part06_01.Menu/src/main/java/Menu.java
+++ vscode/mooc-java-programming-i/part06-Part06_01.Menu/src/main/java/Menu.java
@@ -9,5 +9,23 @@ public class Menu {
         this.meals = new ArrayList<>();
     }
 
-    // implement the required methods here
+    public void addMeal(String meal) {
+        if (!this.meals.contains(meal)) {
+            this.meals.add(meal);
+        } else {
+            System.out.println("This is already on the menu.");
+        }
+    }
+
+    public void printMeals() {
+        if (!meals.isEmpty()) {
+            for (String meal : this.meals) {
+                System.out.println(meal);
+            }
+        }
+    }
+
+    public void clearMenu() {
+        this.meals.removeAll(meals);
+    }
 }
