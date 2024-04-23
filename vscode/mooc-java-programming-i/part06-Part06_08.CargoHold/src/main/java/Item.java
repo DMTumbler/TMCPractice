e1c3e72577999eafbfaa869a0066431eb58c029b
Chris <128728293+DMTumbler@users.noreply.github.com>
2024-04-23 10:26:23 -0400

    Remove unused files and classes

--- vscode/mooc-java-programming-i/part06-Part06_08.CargoHold/src/main/java/Item.java
+++ vscode/mooc-java-programming-i/part06-Part06_08.CargoHold/src/main/java/Item.java
@@ -1,3 +1,25 @@
 public class Item {
 
+    private String name;
+    private int weight;
+
+    public Item(String name, int weight) {
+        this.weight = weight;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return this.name + " (" + this.weight + ")";
+
+    }
 }
--- vscode/mooc-java-programming-i/part06-Part06_08.CargoHold/src/main/java/Main.java
+++ vscode/mooc-java-programming-i/part06-Part06_08.CargoHold/src/main/java/Main.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        // You can use the main to test your classes!
+        String userInput;
+        Scanner scanner = new Scanner(System.in);
+        // String userInput = scanner.nextLine();
+        userInput = "First Book";
+        Item book = new Item(userInput, 12);
+        System.out.println(book.getName());
+
+        System.out.println(book.getWeight());
+        System.out.println(book);
+
     }
 
 }
